7a2ddfbabc132f79b9a5cb47249124500447e9d6
okan kozan <okankzn11@hotmail.com>
2023-06-18 23:49:37 +0300

    first commit

---
+++ src/components/Layout/HeaderCartButton.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/CartProvider";
+import CartIcon from "../Cart/CartIcon";
+import "./HeaderCartButton.css";
+
+const HeaderCartButton = ({ onShowCart }) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalItemsInCart = cartCtx.items.reduce((accumulator, currentItem) => {
+    return accumulator + currentItem.amount;
+  }, 0);
+
+  return (
+    <button className="button" onClick={onShowCart}>
+      <span className="icon">
+        <CartIcon />
+      </span>
+      <span>Sepetim</span>
+      <span className="badge">{totalItemsInCart}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,16 @@
+import "./Products.css";
+import products from "../../productData";
+import ProductItem from "./ProductItem";
+
+const Products = () => {
+  const productList = products.map((product) => (
+    <ProductItem key={product.id} product={product}></ProductItem>
+  ));
+  return (
+    <main className="products-wrapper">
+      <ul className="products">{productList}</ul>
+    </main>
+  );
+};
+
+export default Products;
